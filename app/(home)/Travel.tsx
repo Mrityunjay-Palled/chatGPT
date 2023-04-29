a1974e599e81f28ad58f868a37ffac3a686df70c
Mrityunjay-Palled <113463073+Mrityunjay-Palled@users.noreply.github.com>
2023-04-29 13:45:52 +0530

    getting data from db

--- app/(home)/Travel.tsx
+++ app/(home)/Travel.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Card from 'app/(shared)/Card'
+import {Post} from "@prisma/client"
 
-type Props = {}
+type Props = {
+    travelPosts:Array<Post>
+}
 
-function Travel(props: Props) {
+function Travel({travelPosts}: Props) {
   return (
     <section className='mt-10'>
       <hr className='border-1'/>
@@ -16,19 +19,23 @@ function Travel(props: Props) {
       <div className='sm:flex justify-between gap-8'>
       <Card
           className="bg-wh-500 basis-1/3 mt-5 sm:mt-0 "
+          post={travelPosts[0]}
           imageHeight="h-80"
         />
       <Card
           className="bg-wh-500 basis-1/3 mt-5 sm:mt-0 "
+          post={travelPosts[1]}
           imageHeight="h-80"
         />
       <Card
           className="bg-wh-500 basis-1/3 mt-5 sm:mt-0 "
+          post={travelPosts[2]}
           imageHeight="h-80"
         />
       </div>
       <Card
           className="bg-wh-500 sm:flex justify-between items-center gap-3 mt-7 mb-5"
+          post={travelPosts[3]}
           imageHeight="h-80"
         />
     </section>
